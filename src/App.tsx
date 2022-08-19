7090df161d6f7eb79054a79f6219a2223304f03a
Juan Felipe Ramos Correa <jramoscorrea@twilio.com>
2022-08-19 13:08:45 -0500

    Blog file added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Routes, Route, Outlet, Navigate } from "react-router-dom";
 import Header from "./Header/Header";
 import Home from "./Home/Home";
 import About from "./About/About";
-import Extra from "./Extra/Extra";
+import Blog from "./Blog/Blog";
 import NoMatch from "./NoMatch/NoMatch";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
-        <Route path="extra" element={<Extra />} />
+        <Route path="blog" element={<Blog />} />
         <Route path="not-found" element={<NoMatch />} />
         <Route path="*" element={<Navigate to="not-found" replace />} />
       </Routes>
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,12 +3,14 @@ import "./Header.css";
 
 import { Link } from "react-router-dom";
 
+
+
 function Header() {
   return (
     <div className="Header">
       <Link to="/">Luis Castellanos</Link>
       <Link to="/about">About me</Link>
-      <Link to="/extra">Extra</Link>
+      <Link to="/extra">Blog</Link>
     </div>
   );
 }
